41c1886a0b6728c5576f44a0a728829ae1610826
Olesya Galkina <146961854+olesyagalkina@users.noreply.github.com>
2024-08-23 16:50:59 -0300

    Create vector-index.py

---
+++ vector-index.py
@@ -0,0 +1,34 @@
+import pymongo
+from pymongo.mongo_client import MongoClient
+from pymongo.operations import SearchIndexModel
+
+# Connect to your Atlas deployment
+
+uri = "mongodb+srv://olegalkina:6uyfIuCq0dB1rKMI@@myatlasclusteredu.pnon21i.mongodb.net/?retryWrites=true&w=majority&appName=myAtlasClusterEDU""
+
+client = MongoClient(uri)
+
+# Access your database and collection
+
+database = client["movies"]
+collection = database["movies_records"]
+
+from pymongo.operations import SearchIndexModel
+
+search_index_model = SearchIndexModel(
+  definition={
+    {
+ "fields": [
+   {
+     "numDimensions": 256,
+     "path": "embedding",
+     "similarity": "cosine",
+     "type": "vector"
+    }
+   ]
+  }
+ }
+)
+
+result = collection.create_search_index(model=search_index_model)
+print(result)
